364305e0858f244936560e94d782ac5df98c3639
greenese <31932398+greenese@users.noreply.github.com>
2019-04-11 09:27:36 -0400

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -10,7 +10,7 @@ function GameManager(){
 
     var grid = new Grid(22, 10);
     var rpg = new RandomPieceGenerator();
-    var ai = new TetrisBot(0.619362, 0.321452, 0.40323);
+    var ai = new AI(0.294783, 0.529288, 0.574742, 0.550118);
     var workingPieces = [null, rpg.nextPiece()];
     var workingPiece = null;
     var isAiActive = true;
@@ -23,7 +23,7 @@ function GameManager(){
         return 'rgb(' + ((v >> 16) & 0xFF) + ',' + ((v >> 8) & 0xFF) + ',' + (v & 0xFF) + ')';
     }
 
-    function redrawGridCanvas(workingPieceVerticalOffset = 0) {
+    function redrawGridCanvas(workingPieceVerticalOffset = 0){
         gridContext.save();
 
         // Clear
